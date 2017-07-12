4003863c11cffcde40419ae52dc6abc50e54e0fa
neilASC4 <cmantle721@gmail.com>
2017-07-12 12:06:29 -0400

    LunchTime

---
+++ Neil-Variables/pokemonfunctions.js
@@ -0,0 +1,11 @@
+function randLetter() {
+    var Alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    var RandomABC = Alphabet[Math.floor(Math.random()*27)];
+    return RandomABC
+}
+console.log(randLetter())
+
+function randWord() {
+    
+}
+//stringlength
