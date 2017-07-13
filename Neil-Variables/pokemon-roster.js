9d6b9b2afdafd58a8edadfe1e6262c7030ce6b2e
neilASC4 <cmantle721@gmail.com>
2017-07-13 11:58:26 -0400

    Grrtt

---
+++ Neil-Variables/pokemon-roster.js
@@ -0,0 +1,13 @@
+var roster = ["NAME:","TYPE:","HP:","DEF:","ATK:","LEGEND:"]
+var Pokemon = []
+Pokemon[0] = ["Mewtwo","Phychic","325","166","281","True"]
+Pokemon[1]
+
+
+var printRoster = '';
+
+for (var i=0; i<6; i++){
+    printRoster = printRoster + (roster[i] + Pokemon[0]);
+}
+
+console.log(printRoster);
